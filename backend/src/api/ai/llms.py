90927924cfb8f8f5fb1b0f384b8f8f5224d45ea6
Kayode Oladipo <khaox07@gmail.com>
2025-08-06 10:38:12 +0100

    chore: updates

---
+++ backend/src/api/ai/llms.py
@@ -0,0 +1,25 @@
+from langchain_openai import ChatOpenAI
+import os
+
+
+OPENAI_BASE_URL = os.environ.get("OPENAI_BASE_URL") or None
+OPENAI_MODEL_NAME = os.environ.get("OPENAI_MODEL_NAME") or 'gpt-4o'
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+
+if not OPENAI_API_KEY:
+    raise NotImplementedError("OPENAI_API_KEY is required")
+
+
+def get_openai_llm():
+    openai_params = {
+        "model": OPENAI_MODEL_NAME,
+        "api_key": OPENAI_API_KEY
+    }
+
+
+    if OPENAI_BASE_URL:
+        openai_params["base_url"] = OPENAI_BASE_URL
+
+    llm_base = ChatOpenAI(**openai_params)
+    
+    return llm_base
--- backend/src/api/chat/routing.py
+++ backend/src/api/chat/routing.py
@@ -3,6 +3,8 @@ from sqlmodel import Session, select
 from .models import ChatMessagePayload, ChatMessage, ChatMessageList
 from typing import List
 from api.db import get_session
+from api.ai.services import generate_email_message
+from api.ai.schemas import EmailMessageSchema
 
 router = APIRouter()
 
@@ -12,19 +14,24 @@ def chat_health():
     return {"hello": "world"}
 
 
-@router.post("/", response_model=ChatMessage)
+@router.post("/", response_model=EmailMessageSchema)
 def create_message(
     payload: ChatMessagePayload,
     session: Session = Depends(get_session)
 ):
     data = payload.model_dump()
-    obj = ChatMessage.model_validate(data)
+    
+    try:
+        obj = ChatMessage.model_validate(data)
+    except Exception as e:
+        print(e)
 
     session.add(obj)
     session.commit()
-    session.refresh(obj)
+    # session.refresh(obj)
 
-    return obj
+    response = generate_email_message(payload.message)
+    return response
 
 
 @router.get("/recent/", response_model=List[ChatMessageList])
